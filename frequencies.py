07cf12963adc292a9128a9d59a6bf2b0d693bcbb
Joseph Magen <josephsmagen@gmail.com>
2022-10-05 09:53:27 +0100

    Implement frequencies procedure

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+# Return a dictionary mapping items onto their frequencies
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Iterate through all items
+    for item in items:
+        # Convert all items into strings
+        itemStr = str(item)
+        # Increment the count for each item by 1
+        if itemStr in frequencies:
+            frequencies[itemStr] += 1
+        # Or set it to 1 if the counter has not yet been initialised
+        else:
+            frequencies[itemStr] = 1
+    # Return the dictionary
     return frequencies
